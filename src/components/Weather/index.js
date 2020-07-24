69e164c00cfd633fe244d26a45df13f33d09a783
Setoa <diqzk528@naver.com>
2020-07-24 20:11:25 +0900

    little mistake catch

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,21 +1,5 @@
 import React from "react";
 const API_WEATHER="http://localhost:8080/weather-service/weathers?cityName=";
-// Stateless component
-// const Weather = (props) => {
-//     // 1 .
-//     // class 형태로 변경 후 fetch 선택 도시의 날씨
-//     // 2 .
-//     // react Hook << 검색
-//     // state, setState
-//     const { cityName } = props.match.params;
-//     console.log(cityName);
-//     return (
-//         <div>
-//             <h1>Weather : {cityName}</h1>
-//             <p>Here is front page.!</p>
-//         </div>
-//     );
-// };
 
 class Weather extends React.Component {
     state = {
@@ -30,7 +14,6 @@ class Weather extends React.Component {
         const weatherData = fetch(API_WEATHER+cityName)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
             this.setState({
                 cityName: cityName,
                 main:data.weather[0].main,
@@ -43,7 +26,7 @@ class Weather extends React.Component {
         const {cityName, main, description, celcius} = this.state;
         return(
             <div>
-                <h2>Current Weather of {cityName}</h2>
+                <h1>Weather : {cityName}</h1>
                 <ul>
                     <li>도시이름 : {cityName}</li>
                     <li>날씨 : {main}</li>
